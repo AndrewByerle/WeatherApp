a489d44fd553ca33ef72c6cb519f8281ff4c13b1
Andrew Byerle <andran@live.unc.edu>
2022-12-21 14:00:42 -0500

    Initial Commit

---
+++ Swift-Weather/Swift_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_WeatherApp.swift
+//  Swift-Weather
+//
+//  Created by Andrew Byerle on 12/21/22.
+//
+
+import SwiftUI
+
+@main
+struct Swift_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
